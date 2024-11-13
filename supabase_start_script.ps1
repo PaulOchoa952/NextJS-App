16a6ed861c06ec03a5fe0ff537f8a7549681fd0b
paul <paalochoame@ittepic.edu.mx>
2024-11-13 12:44:52 -0700

    update script

--- supabase_start_script.ps1
+++ supabase_start_script.ps1
@@ -1,6 +1,23 @@
 # Navigate to the project directory
 cd "C:\Users\p_a_u\desktop\supabase"
 
-npx supabase stop
-# Start the Supabase instance
-npx supabase start
+# Check if Docker Desktop is running
+try {
+    docker info > $null 2>&1
+    if ($LASTEXITCODE -eq 0) {
+        Write-Output "Docker Desktop is running."
+
+        # Stop the Supabase instance
+        npx supabase stop
+
+        # Start the SupSabase instance
+        npx supabase start
+
+        # Navigate to the Next.js project directory
+        cd "C:\Users\p_a_u\Desktop\Next-Crud\my-next-app"
+    } else {
+        Write-Output "Docker Desktop is not running."
+    }
+} catch {
+    Write-Output "Docker Desktop is not running."
+}
--- supabase_stop_script.ps1
+++ supabase_stop_script.ps1
@@ -4,3 +4,5 @@ cd "C:\Users\p_a_u\desktop\supabase"
 #stop the Supabase instance
 npx supabase stop
 
+
+cd "C:\Users\p_a_u\Desktop\Next-Crud\my-next-app"
